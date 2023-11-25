445ac5c43da0f0bea123a56462cb7962d063cc92
NineFlint <tom@tcousins.com>
2023-11-26 02:25:31 +1100

    Add limited search for requesters

--- Commands/Get-FreshUser.ps1
+++ Commands/Get-FreshUser.ps1
@@ -1,11 +1,14 @@
-
 function Get-FreshUser {
     [CmdletBinding(DefaultParameterSetName='All')]
     Param(        
         [Alias('primary_email','user_id','id')]
         [ValidateNotNullOrEmpty()]
         [Parameter(Position=0,ParameterSetName='User',ValueFromPipelineByPropertyName)]
-        [String]$User 
+        [String]$User,
+
+        [ValidateNotNullOrEmpty()]
+        [Parameter(ParameterSetName='Search')]
+        [String]$Search
     )
 
     process {
@@ -17,12 +20,20 @@ function Get-FreshUser {
             }
         }
         
-        switch($User) {
-            {$_ -as [Int64]             } { $params.Endpoint += "/$User"  }
-            {$_ -as [MailAddress]       } { $params.Body['email'] = $User }
-            {[String]::IsNullOrEmpty($_)} { } # default all, no change
-            default {
-                throw "Invalid parameter 'User': '$User' must be id or email."
+        switch($PSCmdlet.ParameterSetName) {
+            'User' {
+                switch($User) {
+                    {$_ -as [Int64]             } { $params.Endpoint += "/$User"  }
+                    {$_ -as [MailAddress]       } { $params.Body['email'] = $User }
+                    {[String]::IsNullOrEmpty($_)} { } # default all, no change
+                    default {
+                        throw "Invalid parameter 'User': '$User' must be id or email."
+                    }
+                }
+            }
+            'Search' {
+                # despite saying email, filters by firstname and lastname as well. the query syntax for requesters kindof sucks
+                $params.Body['query'] = "~[primary_email]:'$Search'"
             }
         }
 
