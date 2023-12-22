de8952cfca4090836310153df1885987a6d401a9
NineFlint <tom@tcousins.com>
2023-12-22 19:06:45 +1100

    cache problems, stripping current functionality

--- Private/fresh_cache.ps1
+++ Private/fresh_cache.ps1
@@ -1,27 +1,12 @@
-$global:FreshCache = [PSCustomObject]@{
-    #Locations = [Collections.Generic.Dictionary[String, [Int64[]]]]::new()
-    #Departments = [Collections.Generic.Dictionary[String, Int64]]::new()
-    #Groups = [Collections.Generic.Dictionary[String, Int64]]::new()
-    Requesters = [Collections.Generic.Dictionary[MailAddress, Int64]]::new()
-}
-
-$FreshCache |
-Add-Member -MemberType ScriptMethod -Name Clear -Value {
-    #$this.Locations.Clear()
-    #$this.Departments.Clear()
-    #$this.Groups.Clear()
-    $this.Requesters.Clear()
-}
-
-function user_email_to_id {
-    Param($email)
-    if($FreshCache.Requesters.ContainsKey($email)) {
-        $FreshCache.Requesters[$email]
-    } else {
-        $user = Get-FreshUser -User $email
-        if($user) {
-            $FreshCache.Requesters[$user.primary_email] = $user.id
-            $FreshCache.Requesters[$user.primary_email]
-        }
+Set-Variable -Name 'FreshCache' -Scope Global -Value ([PSCustomObject]@{
+    User = [PSCustomObject]@{
+        FromId = @{}
+        FromMail = @{}
     }
+})
+
+$global:FreshCache | Add-Member -MemberType ScriptMethod -Name 'AddUser' -Value {
+    Param($user)
+    $this.User.FromId[$user.id] = $user
+    $this.User.FromMail[$user.primary_email] = $user
 }
