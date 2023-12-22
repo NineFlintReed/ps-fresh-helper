6740818967834f5f4e62e5e08e8a97f95626f5a6
NineFlint <tom@tcousins.com>
2023-12-22 19:08:58 +1100

    simplify Get-FreshUser and strip out pipeline support (for now)

--- Commands/Get-FreshUser.ps1
+++ Commands/Get-FreshUser.ps1
@@ -1,53 +1,108 @@
+function Get-FreshUser_ViaId {
+    Param($user_id)
+    $params = @{
+        Method = 'GET'
+        Endpoint = "/api/v2/requesters/$user_id"
+        Body = @{
+            include_agents='true'
+        }
+    }
+    $result = (Invoke-FreshRequest @params).requester
+    $FreshCache.AddUser($result)
+    $result
+}
+
+
+function Get-FreshUser_ViaEmail {
+    Param($email)
+    $params = @{
+        Method = 'GET'
+        Endpoint = "/api/v2/requesters"
+        Body = @{
+            email = $email
+            include_agents='true'
+        }
+    }
+    $result = (Invoke-FreshRequest @params).requesters
+    if(-not $result) {
+        Write-Error -Message "User email '$email' not found." -ErrorAction 'Stop' -Category ObjectNotFound
+    }
+    $FreshCache.AddUser($result)
+    $result
+}
+
+
+function Get-FreshUser_All {
+    $params = @{
+        Method = 'GET'
+        Endpoint = "/api/v2/requesters"
+        Body = @{
+            include_agents='true'
+            per_page = 100
+        }
+    }
+    Invoke-FreshRequest @params |
+    Select-Object -ExpandProperty requesters |
+    ForEach-Object {
+        $FreshCache.AddUser($_)
+        $_
+    }
+}
+
+
+function Get-FreshUser_Search {
+    Param($searchterm)
+    $params = @{
+        Method = 'GET'
+        Endpoint = "/api/v2/requesters"
+        Body = @{
+            # despite saying email, filters by firstname and lastname as well. the query syntax for requesters kindof sucks
+            query = "~[primary_email]:'$searchterm'"
+            include_agents='true'
+            per_page = 100
+        }
+    }
+
+    Invoke-FreshRequest @params |
+    Select-Object -ExpandProperty requesters |
+    ForEach-Object {
+        $FreshCache.AddUser($_)
+        $_
+    }
+}
+
+
 function Get-FreshUser {
     [CmdletBinding(DefaultParameterSetName='All')]
-    Param(        
-        [Alias('primary_email','user_id','id')]
+    Param(
         [ValidateNotNullOrEmpty()]
-        [Parameter(Position=0,ParameterSetName='User',ValueFromPipelineByPropertyName)]
-        [String]$User,
+        [Parameter(Position=0,ParameterSetName='User')]
+        $User,
 
         [ValidateNotNullOrEmpty()]
         [Parameter(ParameterSetName='Search')]
         [String]$Search
     )
-
-    process {
-        $params = @{
-            Method = 'GET'
-            Endpoint = "/api/v2/requesters"
-            Body = @{
-                include_agents='true'
-            }
-        }
-        
-        switch($PSCmdlet.ParameterSetName) {
-            'User' {
-                switch($User) {
-                    {$_ -as [Int64]             } { $params.Endpoint += "/$User"  }
-                    {$_ -as [MailAddress]       } { $params.Body['email'] = $User }
-                    {[String]::IsNullOrEmpty($_)} { } # default all, no change
-                    default {
-                        throw "Invalid parameter 'User': '$User' must be id or email."
-                    }
+    
+    switch($PSCmdlet.ParameterSetName) {
+        'User' {
+            switch($User) {
+                {$_ -as [Uint64]} {
+                    Get-FreshUser_ViaId $User
+                }
+                {$_ -as [MailAddress]} {
+                    Get-FreshUser_ViaEmail $User
+                }
+                default {
+                    Write-Error "$($User.GetType()) invalid for param 'User'. Must use either ID or email" -ErrorAction Stop -Category InvalidArgument
                 }
             }
-            'Search' {
-                # despite saying email, filters by firstname and lastname as well. the query syntax for requesters kindof sucks
-                $params.Body['query'] = "~[primary_email]:'$Search'"
-            }
         }
-
-        if($PSCmdlet.ParameterSetName -eq 'User') {
-            $result = Invoke-FreshRequest @params |
-            Select-Object -ExpandProperty 'requester*'
-            if(-not $result) {
-                Write-Error -Message "User email '$User' not found." -ErrorAction 'Stop' -Category ObjectNotFound
-            } else {
-                $result
-            }
-        } else {
-            Invoke-FreshRequest @params |
-            Select-Object -ExpandProperty 'requester*'
+        'Search' {
+            Get-FreshUser_Search $Search
+        }
+        'All' {
+            Get-FreshUser_All
         }
     }
 }
