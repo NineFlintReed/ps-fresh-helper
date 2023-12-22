b2622f7bd536740e40873a951fd9db2b07e7e8b7
NineFlint <tom@tcousins.com>
2023-12-22 20:15:36 +1100

    add asset types and preloading

--- Private/fresh_cache.ps1
+++ Private/fresh_cache.ps1
@@ -9,6 +9,17 @@ Set-Variable -Name 'FreshCache' -Scope Global -Value ([PSCustomObject]@{
     }
 })
 
+
+# preloading some parts of the cache for use with autocomplete etc
+Invoke-FreshRequest -Method 'GET' -Endpoint "/api/v2/asset_types" |
+Select-Object -ExpandProperty 'asset_types' |
+ForEach-Object {
+    $FreshCache.AssetType.FromId[$_.id] = $_
+    $FreshCache.AssetType.FromName[$_.name] = $_
+}
+
+
+# utility methods for retrieving cached properties
 $global:FreshCache |
 Add-Member -MemberType ScriptMethod -Name 'AddUser' -Value {
     Param($user)
@@ -21,5 +32,13 @@ Add-Member -MemberType ScriptMethod -Name 'GetUser' -Value {
         $this.User.FromId[$user]
     } elseif($user -as [MailAddress]) {
         $this.User.FromMail[$user]
+    }
+} -PassThru |
+Add-Member -MemberType ScriptMethod -Name 'GetAssetType' -Value {
+    Param($assettype)
+    if($assettype -as [Uint64]) {
+        $this.AssetType.FromId[$assettype]
+    } elseif($user -as [String]) {
+        $assettype.AssetType.FromName[$assettype]
     }
 }
